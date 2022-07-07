16e180ee98dea4eadd63366a8179fffb5266da11
Jennifer Grohe <jennifer.grohe@gmail.com>
2022-07-07 20:58:36 +0900

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'Spacious Large Private Room By Park Avenue',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+
+Flat.create!(
+  name: 'Black Lake Cabin',
+  address: '478 Glen Falls Road Miami',
+  description: 'Big Master bed with private bath near Miami Beach',
+  price_per_night: 90,
+  number_of_guests: 5
+)
+
+Flat.create!(
+  name: 'Rockaway beach Villa',
+  address: '3062 Calvin Street Illios',
+  description: 'Cozy Private Room for Rent with Beautiful view',
+  price_per_night: 80,
+  number_of_guests: 2
+)
+
+Flat.create!(
+  name: 'Chez Christopher',
+  address: '30 Walkers Ridge Way Los Angeles',
+  description: 'Beautiful loft in Los Angeles, minutes to downtown',
+  price_per_night: 100,
+  number_of_guests: 6
+)
